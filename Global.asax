bdb6a46ab27faaad879782fe255cf3c5fd593f82
bashar61584 <106843122+bashar61584@users.noreply.github.com>
2022-06-04 00:48:59 +0200

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SchoolMS.MvcApplication" Language="C#" %>
---
+++ ProgramTitle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SchoolMS
+{
+    public class ProgramTitle
+    {
+        public static string Header = "Hira Model School Mandani";
+        public static string Phone = "077451289 -- 03459411083";
+        public static string Address= "Gardez City, Baghaka";
+        public static string Email= "bashar6158@gmail.com";
+    }
+}
